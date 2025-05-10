2319d340e77e5880d1d5dd6feffcd418b24544ce
Pedro Hernandez Rosales <pedro900630@gmail.com>
2025-05-10 18:08:41 -0300

    Add class-validator and class-transformer dependencies; implement CreateProductDto with validation rules

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); 
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(envs.port);
   console.log(`Application is running on port ${envs.port}`);
 }
