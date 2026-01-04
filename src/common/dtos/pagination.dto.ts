8f26b6983f61d6b30a8d1220df8eaf842ae9481b
pedro900630 <pedro900630@gmail.com>
2026-01-04 16:32:01 -0300

    feat(dtos): enhance pagination and product DTOs with ApiProperty decorators

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    required: false,
+    description: 'Number of items to return',
+    example: 10,
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number) // enableImplicitConversions: true
   limit?: number;
 
+  @ApiProperty({
+    required: false,
+    description: 'Number of items to skip',
+    example: 0,
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number) // enableImplicitConversions: true
